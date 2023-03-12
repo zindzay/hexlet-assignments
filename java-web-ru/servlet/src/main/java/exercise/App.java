7a0e0bab42edecd0716d4c56b630d571b9a7965d
@hexlet/cli <support@hexlet.io>
2023-03-12 21:29:57 +0300

    submit java-web-ru/servlet

--- java-web-ru/servlet/src/main/java/exercise/App.java
+++ java-web-ru/servlet/src/main/java/exercise/App.java
@@ -26,8 +26,8 @@ public class App {
         Context ctx = app.addContext("", new File(".").getAbsolutePath());
 
         // BEGIN
-        Tomcat.addServlet(ctx, "WelcomeServlet", new WelcomeServlet());
-        ctx.addServletMappingDecoded("/", "WelcomeServlet");
+        Tomcat.addServlet(ctx, WelcomeServlet.class.getSimpleName(), new WelcomeServlet());
+        ctx.addServletMappingDecoded("/", WelcomeServlet.class.getSimpleName());
         // END
 
         return app;
