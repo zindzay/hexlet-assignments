aeb74045d647d14970d4b060b6462ebc28e38d84
@hexlet/cli <support@hexlet.io>
2023-04-23 09:21:33 +0300

    submit java-spring-ru/spring-orm

---
+++ java-spring-ru/spring-orm/src/main/java/exercise/controller/PeopleController.java
@@ -0,0 +1,50 @@
+package exercise.controller;
+
+import exercise.model.Person;
+import exercise.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/people")
+public class PeopleController {
+
+    // Автоматически заполняем значение поля
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping(path = "/{id}")
+    public Person getPerson(@PathVariable long id) {
+        return this.personRepository.findById(id);
+    }
+
+    @GetMapping(path = "")
+    public Iterable<Person> getPeople() {
+        return this.personRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    public void addPeople(@RequestBody Person person) {
+        this.personRepository.save(person);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletePeople(@PathVariable long id) {
+        this.personRepository.deleteById(id);
+    }
+
+    @PatchMapping(path = "/{id}")
+    public void updatePeople(@PathVariable long id, @RequestBody Person person) {
+        person.setId(id);
+        this.personRepository.save(person);
+    }
+    // END
+}
